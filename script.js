60a784d7a268cc4653840cdbcd679d45e6b09d7e
farrellh117 <farrelhafidi035@gmail.com>
2025-06-17 21:00:41 +0700

    Update

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".canvas-container");
+const pixelDisplay = document.getElementById("pixel");
+const slider = document.getElementById("myRange");
+const gridSize = slider.value;
+
+slider.addEventListener("input", () => {
+    // Ambil nilai baru slider tiap kali digeser
+    const newSize = slider.value;
+    pixelDisplay.textContent = newSize
+    createGrid(newSize);
+})
+
+function createGrid(size) {
+    container.innerHTML = "";
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    const totalBox = size * size;
+
+    for (let i = 0; i < totalBox; i++) {
+        const box = document.createElement("div");
+        box.classList.add("grid-box");
+        container.appendChild(box);
+    }
+}
+
+createGrid(gridSize);
