cb40afe1e172857f3f38fdd73551e87db05f1f25
farrellh117 <farrelhafidi035@gmail.com>
2025-06-29 22:46:38 +0700

    Tambah event listerer untuk fitur draw

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const container = document.querySelector(".canvas-container");
+// DOM untuk membuat grid
+const container = document.getElementsByClassName("canvas-container")[0];
 const slider = document.getElementById("grid-size");
 const pixelDisplay = document.getElementById("pixel");
 const gridSize = slider.value;
 
-// let currentMode = "draw";
-// let drawMode = false;
+// DOM untuk draw
+const drawBtn = document.getElementById("drawBtn");
+const colorInput = document.getElementById("favColor");
+const gridBox = document.getElementsByClassName("grid-box");
+
+let isDrawing = false;
 
 function createGrid(size) {
     container.innerHTML = "";
@@ -23,10 +28,42 @@ function createGrid(size) {
     }
 }
 
+function draw(color) {
+    const boxes = document.getElementsByClassName("grid-box");
+    const selectedColor = color;
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mousedown", () => {
+            isDrawing = true;
+            boxes[i].style.backgroundColor = selectedColor;
+        })
+
+        boxes[i].addEventListener("mouseover", () => {
+            if (isDrawing === true) {
+                boxes[i].style.backgroundColor = selectedColor;
+            }
+        })
+
+        boxes[i].addEventListener("mouseup", () => {
+            isDrawing = false;
+        })
+    }
+}
+
 slider.addEventListener("input", () => {
     const newSize = slider.value;
     pixelDisplay.textContent = newSize;
     createGrid(newSize);
+})
+
+drawBtn.addEventListener("click", () => {
+    const currentColor = colorInput.value;
+    draw(currentColor);
+})
+
+colorInput.addEventListener("change", () => {
+    const newColor = colorInput.value;
+    draw(newColor);
 })
 
 createGrid(gridSize);
